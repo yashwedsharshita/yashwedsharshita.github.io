efa1b5f3755ebfe492398c2f11a002e6a70578a5
yashraj0312 <yashraj0312@gmail.com>
2026-04-12 14:06:43 +0530

    made feedback changes

--- script.js
+++ script.js
@@ -204,8 +204,8 @@ const sectionsData = [
     icon: null,
   },
   {
-    head: "Mrs. Vandana Saxena <br> and <br> Mr. Ashutosh Saxena",
-    bodyText: [`<i class="mb-24">Invite you to celebrate the auspicious union of our beloved son</i>`, "Yash Raj🤵", "<i>with</i>", "Harshita Srivastava👰", `<i class="mt-24">(D/o Mrs. Neelam Srivastava and Late Mr. Sanjay Srivastava)</i>`, `<i class="mt-12">We look forward to your presence and blessings on this joyous occasion...</i>`],
+    head: "<h4>Mrs. Vandana Saxena and Mr. Ashutosh Saxena</h4>",
+    bodyText: [`<i class="mb-24">Invite you to celebrate the auspicious union of our beloved son</i>`, "<h3>Yash Raj🤵", "<i>with</i>", "Harshita Srivastava👰</h3>", `<i class="mt-24">(D/o Mrs. Neelam Srivastava and Late Mr. Sanjay Srivastava)</i>`, `<i class="mt-12">We look forward to your presence and blessings on this joyous occasion...</i>`],
     type: "story",
     imageURL: "assets/images/invite.png", // No image tag found in this section in the attached file
     icon: "❤️",
